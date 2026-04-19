736d2a851221884dab7d87e6439b3010e03252f3
rustamovuz <nodirbekrustamov771@gmail.com>
2026-04-20 01:05:45 +0500

    added

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const greet = require("./greeting");
-const add= require("./math")
-console.log("fayl  wello world");
+//1
+// const { add,subtrac } = require("./math");
+// console.log(add(5,6),subtrac(4,5));
 
-console.log(add(-9,6));
-console.log(greet("Nodirbek"));
+// const fs = require("fs");
+
+//2
+
+
+
+//amaliyot
+// fs.mkdir("__int_tests__", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("__mocks__", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("__tests__", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("coverage", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("dist", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("docs/api", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("public", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("scripts/deployment", { recursive: true }, (err) => { if (err) throw err; });
+// fs.mkdir("src", { recursive: true }, (err) => { 
+//     if (err) throw err; 
+//     console.log("Struktura tayyor!");
+// });
+
 
--- math.js
+++ math.js
@@ -1,8 +1,11 @@
-// function add(a,b) {
-//     return a+b
-// }
+function add(a,b) {
+    return a+b
+}
 
 function subtrac(a,b) {
     return a-b
 }
-module.exports = subtrac
+module.exports = {
+    add,
+    subtrac
+}
