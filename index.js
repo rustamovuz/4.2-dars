ec62373c8eafb92548ca09a64a4f73331fe91f4c
rustamovuz <nodirbekrustamov771@gmail.com>
2026-04-20 09:19:42 +0500

    homework added

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 //1
 // const { add,subtrac } = require("./math");
 // console.log(add(5,6),subtrac(4,5));
-
 // const fs = require("fs");
-
-//2
-
-
 
 //amaliyot
 // fs.mkdir("__int_tests__", { recursive: true }, (err) => { if (err) throw err; });
@@ -22,4 +17,35 @@
 //     console.log("Struktura tayyor!");
 // });
 
+//2
+// const fs = require('fs');
+// fs.writeFileSync('salom.txt', 'Salom, dunyo!');
+// const maulumot = fs.readFileSync('salom.txt', 'utf8');
+// console.log(maulumot);
 
+
+//3
+// const fs = require('fs');
+// const tasodifiyRaqamlar = Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
+// const bufferMaulumot = Buffer.from(tasodifiyRaqamlar.toString());
+// fs.writeFileSync('data.bin', bufferMaulumot);
+// const oqilganBuffer = fs.readFileSync('data.bin');
+// console.log(oqilganBuffer.toString());
+
+//4
+// const fs = require('fs');
+// const path = require('path');
+// fs.mkdirSync('html', { recursive: true });
+// fs.mkdirSync('static', { recursive: true });
+
+// fs.writeFileSync(path.join('html', 'index.html'), '');
+// fs.writeFileSync(path.join('static', 'script.js'), '');
+// fs.writeFileSync(path.join('static', 'script.ts'), '');
+// fs.writeFileSync(path.join('static', 'style.css'), '');
+
+// fs.writeFileSync('go.mod', '');
+// fs.writeFileSync('go.sum', '');
+// fs.writeFileSync('golang-todo-app', '');
+// fs.writeFileSync('main.go', '');
+// fs.writeFileSync('README.md', '');
+// fs.writeFileSync('tsconfig.json', '');
